991c87fe3a89e0c50b66dc2b32f0f76fe3d770d9
Colin Jenkins <op12no2@gmail.com>
2024-09-20 22:41:37 +0100

    Add files via upload

--- trainer.js
+++ trainer.js
@@ -1,4 +1,10 @@
 
+// messy/hakky code
+
+const id = 'srelu5';
+
+console.log(id);
+
 //{{{  lang fold
 /*
 
@@ -6,31 +12,43 @@
 
 //}}}
 
+const ACTI_RELU   = 1;
+const ACTI_CRELU  = 2;
+const ACTI_SRELU  = 3;
+const ACTI_SCRELU = 4;
+
 const fs = require('fs');
 const readline = require('readline');
+const { exec } = require('child_process');
+const path = require('path');
+//const fs = require('fs').promises;
 
-const dataFiles    = ['data/data1.shuf'];
-const hiddenSize   = 128;
-const batchSize    = 500;
-const learningRate = 0.001;
-const K            = 100;
-const acti         = 1;    // relu - see activations fold
-const interp       = 0.5;
+const dataFiles       = ['data/data1.shuf','data/data2.shuf'];
+const hiddenSize      = 75;
+const acti            = ACTI_SRELU;
+const interp          = 0.5;
 
-const reportRate = 50; // mean batch loss freq during epoch
-const lossRate = 50;   // dataset loss freq
-const epochs = 10000;
-const weightsFile = 'data/weights.js';
-const inputSize = 768;
-const outputSize = 1;
+const shuffle         = true;
+const quant           = 100;
+const batchSize       = 500;
+const learningRate    = 0.001;
+const K               = 100;
+const useL2Reg        = false;
+const useAdamW        = false;
+
+const reportRate      = 50; // mean batch loss freq during epoch
+const lossRate        = 50;   // dataset loss freq
+const epochs          = 10000;
+const weightsFile     = 'data/weights_' + id + '.js';
+const weightsFileQ    = 'data/weights_' + id + '_q.js';
+const inputSize       = 768;
+const outputSize      = 1;
 const maxActiveInputs = 32;
-const beta1 = 0.9;
-const beta2 = 0.999;
-const epsilon = 1e-7;
-const useL2Reg = false;
-const l2RegFactor = 0.001;
-const useAdamW = false;
-const weightDecay = 0.01;
+const beta1           = 0.9;
+const beta2           = 0.999;
+const epsilon         = 1e-7;
+const l2RegFactor     = 0.001;
+const weightDecay     = 0.01;
 
 //{{{  line constants
 //
@@ -57,6 +75,50 @@ const PART_WDL        = 11;
 let minLoss = 9999;
 let numBatches = 0;
 
+//{{{  myround
+
+function myround(x) {
+  return Math.sign(x) * Math.round(Math.abs(x));
+}
+
+//}}}
+//{{{  shuffle
+
+async function shuffleFile(filePath) {
+
+  const tempFile = `${filePath}.tmp`;
+
+  return new Promise((resolve, reject) => {
+    // Shuffle the file and write it to a temporary file
+    exec(`shuf ${filePath} > ${tempFile}`, (err, stdout, stderr) => {
+      if (err) {
+        reject(`Error shuffling file: ${stderr}`);
+        return;
+      }
+
+      // After shuffling, move the temporary file to overwrite the original file
+      exec(`mv ${tempFile} ${filePath}`, (err, stdout, stderr) => {
+        if (err) {
+          reject(`Error moving file: ${stderr}`);
+        } else {
+          resolve(`Shuffled: ${filePath}`);
+        }
+      });
+    });
+  });
+}
+
+async function shuffleAllFiles(files) {
+  try {
+    const shufflePromises = files.map(file => shuffleFile(file));
+    const results = await Promise.all(shufflePromises);
+    results.forEach(result => console.log(result));
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+//}}}
 //{{{  createLineStream
 
 async function* createLineStream(filenames) {
@@ -135,39 +197,39 @@ function dscrelu(x) {
 
 function activationFunction(x) {
   switch (acti) {
-    case 1:
+    case ACTI_RELU:
       return relu(x);
-    case 2:
+    case ACTI_CRELU:
       return crelu(x);
-    case 3:
+    case ACTI_SRELU:
       return srelu(x);
-    case 4:
+    case ACTI_SCRELU:
       return screlu(x);
   }
 }
 
 function activationDerivative(x) {
   switch (acti) {
-    case 1:
+    case ACTI_RELU:
       return drelu(x);
-    case 2:
+    case ACTI_CRELU:
       return dcrelu(x);
-    case 3:
+    case ACTI_SRELU:
       return dsrelu(x);
-    case 4:
+    case ACTI_SCRELU:
       return dscrelu(x);
   }
 }
 
 function activationName(x) {
   switch (acti) {
-    case 1:
+    case ACTI_RELU:
       return "relu";
-    case 2:
+    case ACTI_CRELU:
       return "crelu";
-    case 3:
+    case ACTI_SRELU:
       return "srelu";
-    case 4:
+    case ACTI_SCRELU:
       return "screlu";
   }
 }
@@ -200,7 +262,7 @@ function initializeParameters() {
 //}}}
 //{{{  saveModel
 
-function saveModel(loss, params, epochs) {
+function saveModel(loss, params, epochs, q) {
 
   const actiName = activationName(acti);
   const opt      = optiName();
@@ -208,14 +270,19 @@ function saveModel(loss, params, epochs) {
   var o = '//{{{  weights\r\n';
 
   o += 'const net_h1_size     = '  + hiddenSize             + ';\r\n';
+  if (q)
+    o += 'const net_quant       = '  + quant                  + ';\r\n';
+  else
+    o += 'const net_quant       = '  + 1                    + ';\r\n';
   o += 'const net_lr          = '  + learningRate           + ';\r\n';
-  o += 'const net_activation  = "' + actiName               + '";\r\n';
+  o += 'const net_activation  = '  + actiName               + ';\r\n';
   o += 'const net_stretch     = '  + K                      + ';\r\n';
   o += 'const net_interp      = '  + interp                 + ';\r\n';
   o += 'const net_batch_size  = '  + batchSize              + ';\r\n';
   o += 'const net_num_batches = '  + numBatches             + ';\r\n';
   o += 'const net_positions   = '  + numBatches * batchSize + ';\r\n';
   o += 'const net_opt         = "' + opt                    + '";\r\n';
+  o += 'const net_shuffle     = "' + shuffle                + '";\r\n';
   o += 'const net_l2_reg      = '  + useL2Reg               + ';\r\n';
   o += 'const net_epochs      = '  + epochs                 + ';\r\n';
   o += 'const net_loss        = '  + loss                   + ';\r\n';
@@ -224,47 +291,76 @@ function saveModel(loss, params, epochs) {
 
   //{{{  write h1 weights
   
-  o += 'const net_h1_w = Array(768);\r\n';
+  if (!q)
+    o += 'const net_h1_w = Array(768);\r\n';
+  else
+    o += 'const net_h1_w = Array(768);\r\n';
   
   var a = params.W1;
   var a2 = [];
   
   for (var i=0; i < 768; i++) {
     a2 = [];
+    a3 = [];
     const j = i * hiddenSize;
     for (var k=0; k < hiddenSize; k++) {
       a2.push(a[j+k]);
+      a3.push(myround(a[j+k] * quant));
     }
-    o += 'net_h1_w[' + i + '] = new Float32Array([' + a2.toString() + ']);\r\n';
+    if (!q)
+      o += 'net_h1_w[' + i + ']  = new Float32Array([' + a2.toString() + ']);\r\n';
+    else
+      o += 'net_h1_w[' + i + '] = new Int16Array([' + a3.toString() + ']);\r\n';
   }
   
   //}}}
   //{{{  write h1 biases
   
-  var a  = params.b1;
+  var a = params.b1;
   
-  o += 'const net_h1_b = new Float32Array([' + a.toString() + ']);\r\n';
+  if (!q)
+    o += 'const net_h1_b = new Float32Array([' + a.toString() + ']);\r\n';
+  
+  a = params.b1.map(x => myround(x * quant));
+  
+  if (q)
+    o += 'const net_h1_b = new Int16Array([' + a.toString() + ']);\r\n';
   
   //}}}
   //{{{  write o weights
   
   var a = params.W2;
   
-  o += 'const net_o_w = new Float32Array([' + a.toString() + ']);\r\n';
+  if (!q)
+    o += 'const net_o_w = new Float32Array([' + a.toString() + ']);\r\n';
+  
+  a = params.W2.map(x => myround(x * quant));
+  
+  if (q)
+    o += 'const net_o_w = new Int16Array([' + a.toString() + ']);\r\n';
   
   //}}}
   //{{{  write o bias
   
   var a = params.b2;
   
-  o += 'const net_o_b = ' + a.toString() + ';\r\n';
+  if (!q)
+    o += 'const net_o_b = ' + a.toString() + ';\r\n';
+  
+  a = myround(a * quant);
+  
+  if (q)
+    o += 'const net_o_b = ' + a.toString() + ';\r\n';
   
   //}}}
 
   o += '\r\n//}}}\r\n';
   o += '\r\n//}}}\r\n\r\n';
 
-  fs.writeFileSync(weightsFile, o);
+  if (q)
+    fs.writeFileSync(weightsFileQ, o);
+  else
+    fs.writeFileSync(weightsFile, o);
 }
 
 //}}}
@@ -431,8 +527,12 @@ function decodeLine(line) {
           x = 0 + chPce[ch] * 64 + sq;
         else if (chCol[ch] == BLACK)
           x = 384 + chPce[ch] * 64 + sq;
-        else
-          console.log('colour');
+        else {
+          console.log(j,board.length,'colour',board,ch.charCodeAt(0),chCol[ch],'                        ');
+          console.log(j,board.length,'colour',board,ch.charCodeAt(0),chCol[ch]);
+          console.log(line);
+          process.exit();
+        }
         activeIndices.push(x);
         sq++;
       }
@@ -495,9 +595,10 @@ async function train(filenames) {
   let datasetLoss = 0;
 
   numBatches = await calculateNumBatches(filenames);
-  saveModel(0, params, 0);
+  saveModel(0, params, 0, '');
+  saveModel(0, params, 0, 'q');
 
-  console.log('hidden',hiddenSize,'acti',activationName(acti),'stretch',K,'batchsize',batchSize,'lr',learningRate,'interp',interp,'num batches',numBatches,'filtered positions',numBatches*batchSize);
+  console.log(id, 'hidden',hiddenSize,'acti',activationName(acti),'stretch',K,'shuffle',shuffle,'quant',quant,'batchsize',batchSize,'lr',learningRate,'interp',interp,'num batches',numBatches,'filtered positions',numBatches*batchSize);
 
   let t = 0;
 
@@ -538,7 +639,7 @@ async function train(filenames) {
           batchTargets = [];
         
           if (batchCount % reportRate === 0) {
-            process.stdout.write(`Epoch ${epoch + 1}, Batch ${batchCount}/${numBatches}, Mean Batch Loss: ${totalLoss / batchCount}\r`);
+            process.stdout.write(`${id} Epoch ${epoch + 1}, Batch ${batchCount}/${numBatches}, Mean Batch Loss: ${totalLoss / batchCount}\r`);
           }
         }
         
@@ -546,7 +647,7 @@ async function train(filenames) {
       }
     }
     
-    console.log(`Epoch ${epoch + 1} completed. Mean Batch Loss: ${totalLoss / batchCount}`);
+    console.log(`${id} Epoch ${epoch + 1} completed. Mean Batch Loss: ${totalLoss / batchCount}`);
     
     //{{{  calc dataset loss
     
@@ -560,7 +661,7 @@ async function train(filenames) {
         marker = '***';
       }
     
-      console.log(`Dataset Loss after ${epoch + 1} epochs: ${datasetLoss} ${marker}`);
+      console.log(`${id} Dataset Loss after ${epoch + 1} epochs: ${datasetLoss} ${marker}`);
     }
     
     else {
@@ -569,7 +670,11 @@ async function train(filenames) {
     
     //}}}
     
-    saveModel(datasetLoss, params, epoch + 1);
+    saveModel(datasetLoss, params, epoch + 1, '');
+    saveModel(datasetLoss, params, epoch + 1, 'q');
+    
+    if (shuffle)
+      await shuffleAllFiles(dataFiles);
     
     //}}}
   }
